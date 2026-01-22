67f092d339698ffc0c165cfd294f442d024d0eb7
Joaquin <tpja209@gmail.com>
2026-01-22 13:47:40 +0300

    feat(documents): add CRUD routes and project documents endpoint

--- backend/src/documents/documents.controller.ts
+++ backend/src/documents/documents.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateDocumentDto } from './dtos/createDocumentDto';
+import { UpdateDocumentDto } from './dtos/updateDocumentDto';
 
 @Controller('documents')
-export class DocumentsController {}
+export class DocumentsController {
+  @Get('projects/:id')
+  getAllDocumentsFromProject(@Param('id', ParseIntPipe) id: number) {}
+  @Get(':id')
+  getOneDocument(@Param('id', ParseIntPipe) id: number) {}
+  @Post()
+  createDocument(@Body(ValidationPipe) createDocument: CreateDocumentDto) {}
+  @Patch(':id')
+  updateDocument(
+    @Param('id', ParseIntPipe) id: number,
+    @Body(ValidationPipe) updateDocument: UpdateDocumentDto,
+  ) {}
+  @Delete(':id')
+  deleteDocument(@Param('id', ParseIntPipe) id: number) {}
+}
