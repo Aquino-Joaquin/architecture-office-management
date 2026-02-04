ca9ec325a1963197f1ec670ac9c2edd04f6d2412
Joaquin <tpja209@gmail.com>
2026-02-04 16:24:22 +0300

    feat: add Vercel Speed Insights integration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/PagesRoutes";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 import "react-toastify/dist/ReactToastify.css";
 
 import "./i18n/i18n";
@@ -10,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <>
       <RouterProvider router={router} />
+      <SpeedInsights />
     </>
-  </StrictMode>
+  </StrictMode>,
 );
