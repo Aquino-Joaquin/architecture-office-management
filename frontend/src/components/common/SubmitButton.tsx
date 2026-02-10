63c6e1404dea870a299eec0103d91017b6553459
Joaquin <tpja209@gmail.com>
2026-02-10 10:30:41 +0300

    refactor: create reusable SubmitButton component

---
+++ frontend/src/components/common/SubmitButton.tsx
@@ -0,0 +1,30 @@
+import { Button, Spinner } from "flowbite-react";
+import type { TFunction } from "i18next";
+import { HiPencil, HiPlus } from "react-icons/hi";
+export type Props = {
+  isUploading: boolean;
+  isEditMode: boolean;
+  t: TFunction;
+};
+export default function SubmitButton({ isUploading, isEditMode, t }: Props) {
+  return (
+    <div className="flex justify-end">
+      <Button type="submit" disabled={isUploading}>
+        {isUploading ? (
+          <div className="flex items-center gap-2">
+            <Spinner size="sm" />
+            {t("uploading")}
+          </div>
+        ) : isEditMode ? (
+          <>
+            <HiPencil className="mr-2" /> {t("editButton")}
+          </>
+        ) : (
+          <>
+            <HiPlus className="mr-2" /> {t("createButton")}
+          </>
+        )}
+      </Button>
+    </div>
+  );
+}
